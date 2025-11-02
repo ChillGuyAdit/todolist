f13c0f5d068094e3df18c2b65a9d2d4c4cd68629
ChillGuyAdit <aditberusahasukses@gmail.com>
2025-11-02 19:59:20 +0700

    commit1

---
+++ lib/onBoarding.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:todolist/pages/helper.dart';
+
+class onBoarding extends StatefulWidget {
+  const onBoarding({super.key});
+
+  @override
+  State<onBoarding> createState() => _onBoardingState();
+}
+
+class _onBoardingState extends State<onBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor().putih,
+      body: Align(
+        alignment: AlignmentGeometry.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('image/primerLogo.jpg', width: 291, height: 291),
+            SizedBox(height: 40),
+            Text(
+              'Sederhanakan, Atur, dan',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: AppColor().hitam,
+                fontSize: 30,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Kuasai',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: AppColor().hitam,
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Hari Kamu',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: AppColor().primer,
+                  ),
+                ),
+              ],
+            ),
+            Text(''),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/helper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  Color primer = Color(0xFF2196F3);
+  Color kuning = Color(0xFFFFC107);
+  Color hitam = Color(0xFF333333);
+  Color orange = Color(0xFFFF5722);
+  Color putih = Color(0xFFFFFFFF);
+}
